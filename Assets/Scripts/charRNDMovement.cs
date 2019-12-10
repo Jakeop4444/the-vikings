74b986371ae19314ac19e539f098bf47ba8b5cb7
BuildTools <cRudel@github.com>
2019-12-10 18:59:54 -0500

    started to work on moving character to position on mouse click

---
+++ Assets/Scripts/charRNDMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charRNDMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool rndMove;
+    public float speed;
+    GameObject character;
+    IEnumerator coroutine;
+
+    void Start()
+    {
+        rndMove = true;
+        speed = 8.0f;
+        character = this.gameObject;
+        coroutine = rndCharMove();
+        StartCoroutine(coroutine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator rndCharMove()
+    {
+        while (rndMove)
+        {
+            int direction = Mathf.RoundToInt((1 + Random.Range(0f, 9f))); //generates a rnd number between 1 and 10. if the number is less than 8, the char does nothing
+                                                                          //i chose to ignore 10 cuz i felt like 9 was getting picked way more than 10 was
+            var tempPos = character.transform.position;
+            if (direction == 8) // move left
+            {
+                tempPos.x -= speed * Time.deltaTime;
+                character.GetComponent<SpriteRenderer>().flipX = false;
+            }
+            else if (direction == 9) // move right
+            {
+                tempPos.x += speed * Time.deltaTime;
+                character.GetComponent<SpriteRenderer>().flipX = true;
+            }
+            character.transform.position = tempPos;
+            Debug.Log(this.gameObject.name + ": " + direction);
+            yield return new WaitForSecondsRealtime(1);
+        }
+        yield return new WaitForSecondsRealtime(1);
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/leifScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leifScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool shouldLerp;
+
+    public float timeStartedLerping;
+    public float lerpTime;
+
+    public Vector2 startPos;
+    public Vector2 endPos;
+
+    public GameObject charLeif;
+    void Start()
+    {
+        charLeif = this.gameObject;
+        shouldLerp = false;
+        startPos = charLeif.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldLerp)
+        {
+            transform.position = Lerp(startPos, endPos, timeStartedLerping, lerpTime);
+        }
+    }
+
+    public void StartLerping(Vector2 pos)
+    {
+        endPos = pos;
+        timeStartedLerping = Time.time;
+        shouldLerp = true;
+    }
+
+    public Vector3 Lerp(Vector3 start, Vector3 end, float timeStartedLerping, float lerpTime = 3)
+    {
+        float timeSinceStarted = Time.time - timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+        var result = Vector3.Lerp(start, end, percentageComplete);
+        shouldLerp = false;
+        return result;
+    }
+
+}
